c5997f96e02041355f0274c0c8047bb6f3aad38c
embryzon <95128949+embryzon@users.noreply.github.com>
2025-08-30 23:40:32 +0800

    add notice

--- src/components/tab-settings/information/NoFileLoaded.svelte
+++ src/components/tab-settings/information/NoFileLoaded.svelte
@@ -1,7 +1,3 @@
-<script>
-	// import { sessionSettings } from '$lib/stores/session-store.js';
-</script>
-
 <div class="flex w-full flex-col gap-4 text-wrap">
 	<p class="opacity-70 hover:opacity-95">&gt; No MIDI file loaded.</p>
 	<p class="opacity-70 hover:opacity-95">
@@ -9,7 +5,21 @@
 	</p>
 </div>
 
-<!-- Debug -->
-<!-- <pre>
-	{JSON.stringify($sessionSettings, null, 2)}
-</pre> -->
+<div
+	class="flex max-w-4xl flex-col gap-8 rounded-lg bg-secondary-acc/5 px-8 py-4 font-mono text-slate-50/60 outline-dashed outline-slate-50/10"
+>
+	<div class="border-b border-slate-50/40 pb-4">
+		<h3 class="text-center text-xl font-semibold uppercase">Notice</h3>
+		<p class="text-center text-sm font-thin">Currently the app is on the process of development.</p>
+	</div>
+	<div>
+		<p class="text-lg font-semibold uppercase">Status:</p>
+		<p class="font-thin">Integrating the settings to the MIDI Piano canvas renderer.</p>
+	</div>
+	<div>
+		<p class="text-lg font-semibold uppercase">Additions:</p>
+		<ul class="ml-8 list-disc font-thin">
+			<li>Number of Keys now reflects in the canvas.</li>
+		</ul>
+	</div>
+</div>
