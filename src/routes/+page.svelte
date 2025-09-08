fb6b530c9b00de70669821bcab44a47f35d7f8ec
embryzon <95128949+embryzon@users.noreply.github.com>
2025-09-08 18:53:41 +0800

    update UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,38 +13,45 @@
 
 	let items = [
 		{
+			logo: History,
+			label: 'Changelog',
+			subtitle: 'Browse update changelogs',
+			value: 0,
+			component: Changelog
+		},
+		{
 			logo: Info,
 			label: 'MIDI Information',
 			subtitle: 'Specific details about the MIDI file.',
-			value: 0,
+			value: 1,
 			component: Information
 		},
 		{
 			logo: Settings,
 			label: 'Customize',
 			subtitle: 'Set the MIDI video configuration.',
-			value: 1,
+			value: 2,
 			component: Customize
 		},
 		{
 			logo: Sparkles,
 			label: 'Filters',
 			subtitle: 'Add filters and other effects',
-			value: 2,
+			value: 3,
 			component: Filters
 		},
 		{
 			logo: Music,
 			label: 'Audio',
 			subtitle: 'Configure the audio for the video.',
-			value: 3,
+			value: 4,
 			component: Audio
 		},
 		{
 			logo: Video,
 			label: 'Render/Preview',
 			subtitle: 'See how your video looks like before rendering.',
-			value: 4,
+			value: 5,
 			component: Render
 		},
 		{
@@ -52,15 +59,8 @@
 			label: 'Saved Settings',
 			subtitle:
 				'Here are your saved settings (these are stored in the local storage of the browser)',
-			value: 5,
-			component: SavedSettings
-		},
-		{
-			logo: History,
-			label: 'Changelog',
-			subtitle: 'Browse update changelogs',
 			value: 6,
-			component: Changelog
+			component: SavedSettings
 		}
 	];
 </script>
